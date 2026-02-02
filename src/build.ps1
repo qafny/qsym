0c55f06560943b7b768d78efdaac95e4ff7e575a
jump-orange <orangefayecheng@gmail.com>
2026-02-02 15:05:28 -0600

    readme added

--- src/build.ps1
+++
@@ -1,9 +0,0 @@
-# build.ps1
-# runs antlr4 to generate the required python files needed for qafny
-# todo: consider an actual build system
-# todo: check that the antlr version matches the installed python package version
-# todo: maybe version range allowance
-
-# run antlr
-antlr4 -v 4.9.2 -Dlanguage=Python3 -visitor Exp.g4
-# antlr4 -Dlanguage=Python3 -o dafny -visitor dafny/Dafny.g4
